df853847b1c191053e30807a005d2d025a359c0e
niksintelli4ever <92415325+niksintelli4ever@users.noreply.github.com>
2022-09-29 01:42:02 -0400

    Time: 751 ms (22.19%), Space: 16.5 MB (64.18%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        row=len(grid)
+        cols=len(grid[0]) 
+        count=0
+        def dfs(r,c):
+            if r<0 or c<0 or r>=row or c>=cols or grid[r][c]!='1':
+                return
+            grid[r][c]='#'
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
+        
+        for r in range(row):
+            for c in range(cols):
+                if grid[r][c]=='1':
+                    count+=1
+                    dfs(r,c)
+        
+        return count
