e0850e488ecc05253d0e088bef3ee6b101c78178
niksintelli4ever <92415325+niksintelli4ever@users.noreply.github.com>
2022-10-03 00:46:30 -0400

    Time: 510 ms (51.44%), Space: 15.6 MB (54.86%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -7,29 +7,22 @@ class Solution:
         res=[]
         def dfs(r,c,visit,prev):
             if r<0 or c<0 or r>=rows or c>=cols or (r,c) in visit or prev>heights[r][c]:
-                return
+                return 
             visit.add((r,c))
             dfs(r+1,c,visit,heights[r][c])
             dfs(r-1,c,visit,heights[r][c])
             dfs(r,c+1,visit,heights[r][c])
             dfs(r,c-1,visit,heights[r][c])
+        for r in range(rows):
+            dfs(r,0,pacific,heights[r][0])
+            dfs(r,cols-1,atlantic,heights[r][cols-1])
         
         for c in range(cols):
             dfs(0,c,pacific,heights[0][c])
             dfs(rows-1,c,atlantic,heights[rows-1][c])
         
         for r in range(rows):
-            dfs(r,0,pacific,heights[r][0])
-            dfs(r,cols-1,atlantic,heights[r][cols-1])
-        
-        for r in range(rows):
             for c in range(cols):
-                if (r,c) in pacific and (r,c) in atlantic:
+                if (r,c) in atlantic and (r,c) in pacific:
                     res.append([r,c])
-    
         return res
-        
-        
-        
-    
-        
