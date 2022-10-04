e1d37418336ceecd719752f612bc3c91b6543d9c
niksintelli4ever <92415325+niksintelli4ever@users.noreply.github.com>
2022-10-04 17:09:07 -0400

    Time: 1460 ms (47.75%), Space: 19.9 MB (97.11%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -4,11 +4,16 @@ class Solution:
         a=[]
         for i in range(len(points)):
             x,y=points[i]
-            res[i]=math.sqrt(math.pow(x-0,2) + math.pow(y-0,2)),i
+            res[i]=math.sqrt(pow(x-0,2)+pow(y-0,2)),i
         heapify(res)
+        print(res)
         for i in range(k):
             v,ind=heappop(res)
             a.append(points[ind])
+        
         return a
             
+        
+            
+        
         
