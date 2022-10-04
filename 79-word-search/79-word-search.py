6adc83fa9f0f995af6d1c0172d6b285878af2b1d
niksintelli4ever <92415325+niksintelli4ever@users.noreply.github.com>
2022-10-04 03:35:37 -0400

    Time: 5257 ms (76.55%), Space: 14 MB (50.80%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -9,15 +9,15 @@ class Solution:
             if r<0 or c<0 or r>=rows or c>=cols or board[r][c]!=word[i] or (r,c) in visit:
                 return
             visit.add((r,c))
-            a=dfs(r+1,c,i+1) or dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or dfs(r,c-1,i+1)
+            a= dfs(r+1,c,i+1) or  dfs(r-1,c,i+1) or dfs(r,c-1,i+1) or dfs(r,c+1,i+1)
             visit.remove((r,c))
-        
             return a
-        for r in range(rows):
-            for c in range(cols):
-                if dfs(r,c,0):
+        
+        for i in range(rows):
+            for j in range(cols):
+                if dfs(i,j,0):
                     return True
         
         return False
-                    
-            
+        
+        
