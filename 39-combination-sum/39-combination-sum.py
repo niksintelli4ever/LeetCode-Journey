9c19f52672137d63fb098d6e44f82120daef1d5a
niksintelli4ever <92415325+niksintelli4ever@users.noreply.github.com>
2022-10-05 01:34:13 -0400

    Time: 135 ms (53.62%), Space: 14.1 MB (59.36%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,21 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        
-        
         res=[]
-        def combo(i,subset,sum1):
-            if sum1==target:
-                res.append(subset.copy())
-                return
-            if i>=len(candidates):
-                return
-            if sum1>target:
-                return
-            subset.append(candidates[i])
-            combo(i,subset,sum1+candidates[i])
-            subset.remove(candidates[i])
-            combo(i+1,subset,sum1)
         
-        combo(0,[],0)
+        def recursiveFunction(i,visit,sum1):
+            if i>=len(candidates) or sum1>target:
+                return
+            if sum1==target:
+                res.append(visit.copy())
+                return
+            visit.append(candidates[i])
+            recursiveFunction(i,visit,sum1+candidates[i])
+            visit.remove(candidates[i])
+            recursiveFunction(i+1,visit,sum1)
+        
+        recursiveFunction(0,[],0)
         return res
         
