94f128cb578eeec5663305b14350ddcbcb934b56
niksintelli4ever <92415325+niksintelli4ever@users.noreply.github.com>
2022-10-05 23:33:14 -0400

    Time: 134 ms (40.87%), Space: 13.9 MB (85.99%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,8 +5,8 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        l3=ListNode(0)
-        curr=l3
+        dummy=ListNode(0)
+        l3=dummy
         carry=0
         while l1 or l2 or carry:
             v1=l1.val if l1 else 0
@@ -14,10 +14,14 @@ class Solution:
             v3=v1+v2+carry
             carry=v3//10
             v3=v3%10
-            curr.next=ListNode(v3)
-            curr=curr.next
+            l3.next=ListNode(v3)
+            l3=l3.next
+            
             if l1:
                 l1=l1.next
             if l2:
                 l2=l2.next
-        return l3.next
+        
+        return dummy.next
+            
+        
