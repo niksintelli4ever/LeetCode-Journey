1b5082b9d623709ef732f2be2a4dd80843bca9cf
niksintelli4ever <92415325+niksintelli4ever@users.noreply.github.com>
2022-10-06 00:57:10 -0400

    Time: 57 ms (39.77%), Space: 13.9 MB (68.10%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,18 +1,13 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
-        def powmy(x,n):
-            if x==0:
-                return 0
+        def p(x,n):
             if n==0:
                 return 1
             if n%2==0:
-                res=powmy(x*x,n//2)
-            else:
-                res= x*powmy(x*x,n//2)
-            
-            return res
-        res=powmy(x,abs(n))
-        return res if n>=0 else 1/res
-            
-            
+                return p(x*x,n//2)
+            elif n%2:
+                return x* p(x*x,n//2)
+        
+        a=p(x,abs(n))
+        return a if n>0 else 1/a
