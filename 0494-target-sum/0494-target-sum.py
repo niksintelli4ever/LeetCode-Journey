64ad67a7f8ee5438efadb23522aa89021cb785f4
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2022-10-19 17:34:07 -0400

    Time: 1296 ms (8.59%), Space: 41.7 MB (10.21%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -2,13 +2,13 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
         dp = {}
-        
+        n=len(nums)
         def rec(target,n):
             
-            if target == 0 and n==len(nums):
+            if target == 0 and n==0:
                 return 1
             
-            if target!=0 and n==len(nums):
+            if target!=0 and n==0:
                 return 0
             
             key = str(target) + '_' + str(n)
@@ -16,9 +16,9 @@ class Solution:
             if key in dp:
                 return dp[key]
             
-            dp[key] = rec(target+nums[n],n+1) + rec(target-nums[n],n+1)
+            dp[key] = rec(target+nums[n-1],n-1) + rec(target-nums[n-1],n-1)
             
             return dp[key]
         
-        return rec(target, 0)
+        return rec(target,n )
         
