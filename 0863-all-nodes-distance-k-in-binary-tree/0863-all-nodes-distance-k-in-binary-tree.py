df91b394a5b533a275b5d82c1dc5b1e525dfb6d0
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2022-11-01 02:09:12 -0400

    Time: 35 ms (97.42%), Space: 14.3 MB (77.48%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -7,37 +7,34 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        g=defaultdict(list)
-        visit=set()
-        q=deque()
+        g=defaultdict(list)   
         res=[]
-        self.convertTreetoG(root,None,g)
+        q=deque()
+        visit=set()
+        self.TtoG(root,None,g)
         q.append((target,0))
         while q:
             for i in range(len(q)):
-                n,d=q.popleft()
-                visit.add(n)
+                node,d=q.popleft()
+                visit.add(node)
                 if d==k:
-                    res.append(n.val)
-                
-                for neigh in g[n]:
+                    res.append(node.val)
+
+                for neigh in g[node]:
                     if neigh not in visit:
                         q.append((neigh,d+1))
-        
         return res
-        
-        
-        
-    def convertTreetoG(self,node,parent,g):
-            if not node:
-                return
-            if parent:
-                g[node].append(parent)
-            if node.right:
-                g[node].append(node.right)
-                self.convertTreetoG(node.right,node,g)
-            if node.left:
-                g[node].append(node.left)
-                self.convertTreetoG(node.left,node,g)
     
+    def TtoG(self,root,parent,g):
+        if not root:
+            return
+        if parent:
+            g[root].append(parent)
+        if root.right:
+            g[root].append(root.right)
+            self.TtoG(root.right,root,g)
+        if root.left:
+            g[root].append(root.left)
+            self.TtoG(root.left,root,g)
+        
         
