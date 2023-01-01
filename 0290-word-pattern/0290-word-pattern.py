a9c7e3327f92591fbe4b762992bc90cc6041f83f
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-01-01 02:33:21 -0500

    Time: 42 ms (59.26%), Space: 13.9 MB (22.42%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,20 +1,21 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        dict1=defaultdict(set)
+        dict1=defaultdict(list)
         slist = s.split(" ")
         for i in range(len(slist)):
-            dict1[slist[i]].add(i)
-        dict2 = defaultdict(set)
+            dict1[slist[i]].append(i)
+        dict2 = defaultdict(list)
         for i in range(len(pattern)):
-            dict2[pattern[i]].add(i)
+            dict2[pattern[i]].append(i)
+        
+        if dict1.values() == dict2.values():
+            return True
         list1= []
         for i in dict1.values():
             list1.append(i)
-        print(list1)
         list2 = []
         for i in dict2.values():
             list2.append(i)
-        print(list2)
         if list1== list2:
             return True
         return False
