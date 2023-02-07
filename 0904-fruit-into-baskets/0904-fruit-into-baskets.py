5fe93a4255742bc7c08c67c4a1147391c1e30c80
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-02-07 05:08:29 -0500

    Time: 920 ms (81.84%), Space: 20.6 MB (13.87%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,19 +1,20 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        l=0
         dict1={}
-        resLen=0
-        for r in range(len(fruits)):
-            c=fruits[r]
-            dict1[c]=1+dict1.get(c,0)
-            while len(dict1)>2:
-                dict1[fruits[l]]-=1
-                if dict1[fruits[l]]==0:
-                    del dict1[fruits[l]]
-                l+=1
-            resLen=max(resLen,r-l+1)
-        
-        return resLen
+        maxlength=0
+        l=0
+        r=0
+        while r<len(fruits):
+            dict1[fruits[r]]=r
+            if len(dict1)>2:
+                todel=min(dict1.values())
+                del dict1[fruits[todel]]
+                l=todel+1
+            else:
+                length=r-l+1
+                maxlength=max(maxlength,length)
             
-                
+            r+=1
+        
+        return maxlength
         
