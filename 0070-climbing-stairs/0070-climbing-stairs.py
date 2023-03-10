07dafd29aa7c30c8a01b5c7aa904eba97144f7a8
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-10 01:43:41 -0500

    Time: 35 ms (37.37%), Space: 14 MB (7.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,14 +3,14 @@ class Solution:
         dp={}
         
         def climb(n):
-            print(n)
+            # print(n)
             key=str(n)
             if key in dp:
                 return dp[key]
             if n==0:
-                dp[key]=1
+                return 1
             elif n==1:
-                dp[key]=1
+                return 1
             else:
                 dp[key]=climb(n-1)+climb(n-2)
             return dp[key]
