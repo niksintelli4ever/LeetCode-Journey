915c91cfa23ad8d4c3ad2a0309faee3533b7e504
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-12 05:01:10 -0400

    Time: 30 ms (85.55%), Space: 14.4 MB (5.76%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp={}
+        def dfs(n):
+            key=str(n)
+            if key in dp:
+                return dp[key]
+            if n>=len(s):
+                return 1
+            if s[n]=='0':
+                return 0
+            
+            option1=dfs(n+1)
+            if (n+1<len(s) and (s[n]=='1' or s[n]=='2' and s[n+1] in "0123456")):
+                option1+=dfs(n+2)
+            
+            dp[key]=option1
+            return dp[key]
+        return dfs(0)
+        
