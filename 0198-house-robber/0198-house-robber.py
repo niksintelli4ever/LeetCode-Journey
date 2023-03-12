cb2c40782658c9d3288ebc70b17f36a32922086e
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-12 03:32:03 -0400

    Time: 36 ms (44.02%), Space: 13.9 MB (54.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,7 +3,7 @@ class Solution:
         rob1=0
         rob2=0
         for i in nums:
-            temp=max(i+rob1,rob2)
+            temp=max(rob1+i,rob2)
             rob1=rob2
             rob2=temp
         
