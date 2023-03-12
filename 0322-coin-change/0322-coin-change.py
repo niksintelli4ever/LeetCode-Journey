49911a5c727be609c07e3ac8902fb9196da945a5
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-12 03:48:13 -0400

    Time: 4459 ms (5.00%), Space: 340 MB (5.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,49 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp={}
-        a=0
-        n=len(coins)
-        
-        if amount == 0:
-            return 0
-        
-        dp={}
-        
         def coinC(n,amount):
-            
-            # print(n,amount, count)
-            key=str(n) + '_' + str(amount)
-            # print(key)
+            key=str(n)+"_"+str(amount)
             if key in dp:
-                # print("Key in MEMO", dp[key])
                 return dp[key]
-            
-            if amount == 0:
+            if amount==0:
                 return 0
+            if n>=len(coins) or amount<0:
+                return float("inf")
+            option1=min(1+coinC(n,amount-coins[n]),coinC(n+1,amount))
             
-            if n<=0:
-                return float('inf')
-            
-            # min_so_far = float('inf')
-            if coins[n-1]<=amount:
-                # print("IF")
-                dp[key]= min( 1 + coinC(n,amount-coins[n-1]) , coinC(n-1,amount))
-            
-            else:
-                # print("ELSE")
-                dp[key] = coinC(n-1,amount)
-            
-            # dp[key] = min_so_far
-            
+            dp[key]=option1
+        
             return dp[key]
-        
-        
-        a=coinC(n,amount)
-        # print(dp)   
-
-        return -1 if a == float('inf') else a
-            
-            
-                
-            
+        a=coinC(0,amount)   
+        return -1 if a==float("inf") else a
+    
         
