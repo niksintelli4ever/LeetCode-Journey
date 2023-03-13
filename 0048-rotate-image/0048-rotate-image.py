7e22fc77681c0a7d86ca2362530ca3a0e90c94ec
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-13 01:01:13 -0400

    Time: 35 ms (77.17%), Space: 14 MB (20.99%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -6,8 +6,8 @@ class Solution:
         l=0
         r=len(matrix)-1
         while l<r:
-            top,bottom=l,r
             for i in range(r-l):
+                top,bottom=l,r
                 temp=matrix[top][l+i]
                 matrix[top][l+i]=matrix[bottom-i][l]
                 matrix[bottom-i][l]=matrix[bottom][r-i]
