5215c9109643c9e3ae92e10d6da2e5854f6f1f9b
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-12 21:13:58 -0400

    Time: 108 ms (34.90%), Space: 17.1 MB (36.61%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,26 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        visit=set()
         adjacency={i:[] for i in range(numCourses)}
-        for crs,pre in prerequisites:
-            adjacency[crs].append(pre)
+        for pre,crs in prerequisites:
+            adjacency[pre].append(crs)
+        visit=set()
         def dfs(crs):
             if crs in visit:
                 return False
             if adjacency[crs]==[]:
                 return True
             visit.add(crs)
-            for pre in adjacency[crs]:
-                if not dfs(pre):
+            for neigh in adjacency[crs]:
+                if not dfs(neigh):
                     return False
             visit.remove(crs)
             adjacency[crs]=[]
+            
             return True
         
         for i in range(numCourses):
             if not dfs(i):
                 return False
         return True
-            
                 
         
