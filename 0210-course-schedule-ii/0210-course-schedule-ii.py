96fa390f8dd6d63710c3f3732fa1342f4bdb5c51
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-13 00:12:42 -0400

    Time: 108 ms (43.62%), Space: 17.6 MB (18.70%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        visit=set()
+        cycle=set()
+        res=[]
+        adjacency={i:[] for i in range(numCourses)}
+        for pre,crs in prerequisites:
+            adjacency[pre].append(crs)
+        
+        def dfs(crs):
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+            visit.add(crs)
+            cycle.add(crs)
+            for nei in adjacency[crs]:
+                if not dfs(nei):
+                    return False
+            cycle.remove(crs)
+            res.append(crs)
+            return True
+        for i in range(numCourses):
+            if dfs(i)==False:
+                return []
+        
+        return res
+        
