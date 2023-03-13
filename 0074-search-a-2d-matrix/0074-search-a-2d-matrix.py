31b6c1c255ce71e42e873b0cc0760f16dfbdb07c
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-13 03:06:39 -0400

    Time: 42 ms (83.62%), Space: 14.4 MB (80.98%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,31 +3,25 @@ class Solution:
         top=0
         bottom=len(matrix)-1
         while top<=bottom:
-            row=(top+bottom)//2
-            if target>matrix[row][-1]:
-                top=row+1
-            elif target<matrix[row][0]:
-                bottom=row-1
+            mid=(top+bottom)//2
+            if target>matrix[mid][-1]:
+                top=mid+1
+            elif target<matrix[mid][0]:
+                bottom=mid-1
             else:
                 break
-    
-    
         
-     
+        row=mid
         l=0
         r=len(matrix[0])-1
         while l<=r:
             mid=(l+r)//2
-            if target>matrix[row][mid]:
-                l=mid+1
-            elif target<matrix[row][mid]:
-                r=mid-1
-            else:
+            if target==matrix[row][mid]:
                 return True
+            elif target>matrix[row][mid]:
+                l=mid+1
+            else:
+                r=mid-1
         
         return False
-            
-            
-            
-            
         
