748ea07211ad7609491c3e4db95c394f9271b5d0
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-13 01:57:09 -0400

    Time: 889 ms (5.04%), Space: 15.4 MB (18.65%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.py
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -1,17 +1,30 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
         res=[]
-        def dfs(i,word):
-            if i>=len(s):
-                res.append(word)
+        def dfs(i,visit):
+            if len(visit)==len(s):
+                res.append("".join(visit.copy()))
                 return
-            if s[i].isalpha():
-                dfs(i+1,word+s[i])
-                if s[i].islower():
-                    dfs(i+1,word+s[i].upper())
-                if s[i].isupper():
-                    dfs(i+1,word+s[i].lower())
-            else:
-                dfs(i+1,word+s[i])
-        dfs(0,"")
+            if i>=len(s):
+                return
+         
+            for j in range(i,len(s)):
+                if s[j].isalpha():
+                    visit.append(s[j])
+                    dfs(j+1,visit)
+                    visit.pop()
+                    if s[j].isupper():
+                        visit.append(s[j].lower())
+                        dfs(j+1,visit)
+                        visit.pop()
+                    else:
+                        visit.append(s[j].upper())
+                        dfs(j+1,visit)
+                        visit.pop()
+                else:
+                    visit.append(s[j])
+                    dfs(j+1,visit)
+                    visit.pop()
+        dfs(0,[])
         return res
+        
