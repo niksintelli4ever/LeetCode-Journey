3d2847826b96cc1b5b5c40e60fbaa5454bb5f59d
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-12 20:37:51 -0400

    Time: 31 ms (95.20%), Space: 14.3 MB (42.91%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -7,34 +7,42 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        g=defaultdict(list)   
-        res=[]
-        q=deque()
+        g=defaultdict(list)
+        self.tTog(root,None,g)
+        q=deque([[target,0]])
         visit=set()
-        self.TtoG(root,None,g)
-        q.append((target,0))
+        res=[]
         while q:
             for i in range(len(q)):
                 node,d=q.popleft()
-                visit.add(node)
                 if d==k:
                     res.append(node.val)
-
+                visit.add(node)
                 for neigh in g[node]:
                     if neigh not in visit:
-                        q.append((neigh,d+1))
+                        q.append([neigh,d+1])
+        
         return res
+        
+                
     
-    def TtoG(self,root,parent,g):
+    
+    
+    
+    
+    
+    
+    
+    
+    def tTog(self,root,parent,g):
         if not root:
             return
         if parent:
             g[root].append(parent)
-        if root.right:
-            g[root].append(root.right)
-            self.TtoG(root.right,root,g)
         if root.left:
             g[root].append(root.left)
-            self.TtoG(root.left,root,g)
-        
+            self.tTog(root.left,root,g)
+        if root.right:
+            g[root].append(root.right)
+            self.tTog(root.right,root,g)
         
