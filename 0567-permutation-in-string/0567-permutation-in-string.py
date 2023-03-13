8eb2e86b81b5a0e1eeeb27e6f6924a6f66ab4ab8
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-13 19:04:52 -0400

    Time: 80 ms (63.01%), Space: 14 MB (22.94%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,36 +2,35 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1)>len(s2):
             return False
-        s1M=[0]*26
-        s2M=[0]*26
+        s1A=[0]*26
+        s2A=[0]*26
         matches=0
-        l=0
         for i in range(len(s1)):
-            s1M[ord(s1[i])-ord("a")]+=1
-            s2M[ord(s2[i])-ord("a")]+=1
+            s1A[ord(s1[i])-ord("a")]+=1
+            s2A[ord(s2[i])-ord("a")]+=1
+        
         for i in range(26):
-            if s1M[i]==s2M[i]:
+            if s1A[i]==s2A[i]:
                 matches+=1
+        
+        l=0
         for r in range(len(s1),len(s2)):
             if matches==26:
                 return True
             c=s2[r]
             index=ord(c)-ord("a")
-            s2M[index]+=1
-            if s2M[index]==s1M[index]:
+            s2A[index]+=1
+            if s1A[index]==s2A[index]:
                 matches+=1
-            elif s2M[index]-1==s1M[index]:
+            elif s2A[index]-1==s1A[index]:
                 matches-=1
-            
             c=s2[l]
             index=ord(c)-ord("a")
-            s2M[index]-=1
-            if s2M[index]==s1M[index]:
+            s2A[index]-=1
+            if s1A[index]==s2A[index]:
                 matches+=1
-            elif s2M[index]+1==s1M[index]:
+            elif s2A[index]+1==s1A[index]:
                 matches-=1
             l+=1
         return matches==26
             
-          
-        
