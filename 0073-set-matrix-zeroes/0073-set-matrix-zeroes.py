b27f68aaf08d1f78b0f436d3285a397c4ee7d2d2
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-03-14 00:07:01 -0400

    Time: 179 ms (10.09%), Space: 14.7 MB (65.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,31 +1,22 @@
 class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
+    def setZeroes(self, board: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rowzero=False
-        rows=len(matrix)
-        cols=len(matrix[0])
+        rows=len(board)
+        cols=len(board[0])
+        dontaddrows=[]
+        dontaddcols=[]
         for r in range(rows):
             for c in range(cols):
-                if matrix[r][c]==0:
-                    matrix[0][c]=0
+                if board[r][c]==0:
+                    dontaddrows.append(r)
+                    dontaddcols.append(c)
+        for r in range(rows):
+            for c in range(cols):
+                if r in dontaddrows or c in dontaddcols:
+                    board[r][c]=0
+        
+
                     
-                    if r>0:
-                        matrix[r][0]=0
-                    else:
-                        rowzero=True
         
-        for r in range(1,rows):
-            for c in range(1,cols):
-                if matrix[0][c]==0 or matrix[r][0]==0:
-                    matrix[r][c]=0
-        
-        for r in range(rows):
-            for c in range(cols):
-                if matrix[0][0]==0:
-                    matrix[r][0]=0
-        for r in range(rows):
-            for c in range(cols):
-                if rowzero:
-                    matrix[0][c]=0
