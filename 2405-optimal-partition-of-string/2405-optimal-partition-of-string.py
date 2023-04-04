cd29e65ba48327d865b648e8189cddfba7a3269e
Nikhil Chandra <92415325+niksintelli4ever@users.noreply.github.com>
2023-04-03 23:31:58 -0400

    Time: 93 ms (94.33%), Space: 14.5 MB (90.90%) - LeetHub

---
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def partitionString(self, s: str) -> int:
+
+        ans = 0
+        ss = ""
+
+
+        for i in s:
+            if i not in ss:
+                ss += i
+            else:
+                ans += 1
+                ss = i
+
+        
+        return ans + 1
